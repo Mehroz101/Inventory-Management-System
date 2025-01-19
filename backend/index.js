ab8ac52943b8da23aa2ae5c33642b8d4d5a9cf00
Mehroz101 <mehrozfarooq127@gmail.com>
2025-01-19 21:31:53 +0500

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://webtech-inventorysystem.netlify.app/",
-      "http://localhost:5173",
-    ],
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
